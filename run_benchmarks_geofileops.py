465d833317990713285ad953e35816249c3995a6
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-03-02 17:17:00 +0100

    Fix number of cpus used to 12 (#14)

--- run_benchmarks_geofileops.py
+++ run_benchmarks_geofileops.py
@@ -1,7 +1,16 @@
 import benchmarker
 
-if __name__ == "__main__":
+
+def main():
     benchmarker.run_benchmarks(modules=["benchmarks_geofileops"])
 
+    return
+
     # Only run specific benchmark function(s)
-    # benchmarker.run_benchmarks(modules=["benchmarks_geofileops"], functions=["join_by_location_intersects"])
+    benchmarker.run_benchmarks(
+        modules=["benchmarks_geofileops"], functions=["join_by_location_intersects"]
+    )
+
+
+if __name__ == "__main__":
+    main()
